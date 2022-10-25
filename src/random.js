a38bb8bef65a3e5f15ebfb759abc23ea0fdfab97
Connor Bode <connor.bode@gmail.com>
2022-10-24 23:19:10 -0400

    remove references to crypto

--- src/random.js
+++ src/random.js
@@ -1,19 +1,13 @@
 import ChaCha from "./chacha.js";
-import crypto from "crypto";
 
 export function getRandomBytes(n) {
+    // this is modified to make it simpler to
+    // include poseidon hash in Avvy client
+    // libs. it should not be used in any 
+    // functions that require secure randomness.
     let array = new Uint8Array(n);
-    if (process.browser) { // Browser
-        if (typeof globalThis.crypto !== "undefined") { // Supported
-            globalThis.crypto.getRandomValues(array);
-        } else { // fallback
-            for (let i=0; i<n; i++) {
-                array[i] = (Math.random()*4294967296)>>>0;
-            }
-        }
-    }
-    else { // NodeJS
-        crypto.randomFillSync(array);
+    for (let i=0; i<n; i++) {
+        array[i] = (Math.random()*4294967296)>>>0;
     }
     return array;
 }
